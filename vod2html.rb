4c36d48a81b6003091d6fb11fcdf8f2e03db1aef
David <davidpaullyons@gmail.com>
2012-03-29 11:53:07 -0400

    Refactored methods and variable names. Added useful feedback.

--- vod2html.rb
+++ vod2html.rb
@@ -1,63 +1,43 @@
-#######################################################################
-#                                                                     #
-# vod2html v1.0 does not carry any license and is free to be          #
-# distributed or altered. Enjoy!                                      #
-# --                                                                  #
-# David Lyons                                                         #
-#                                                                     #
-#######################################################################
+$EMBED_HTML = "" 
 
-### Edit these variables for your situation
+$BASE_URL = ""
 
-### HTML embed code
-$EMBED = "" # Insert embed HTML into this variable.
-
-### URL base
-$BASE = "" # Flash server url (with trailing slash)
-
-### Do not edit below this line ###
-
-###### Methods ######
-
-### Cleans up and validates user input, or forces resubmission of user input
+def normalize_directory(dir)
+	normalized = dir.gsub('\\', '/')
+	normalized += '/' unless normalized[-1] == '/'
+end
 
 def validate_directory(dir)
 	puts "Enter the full directory path of the flv files." unless dir
-	input =  dir || gets.chomp
-	input.gsub!('\\', '/')
-	input += '/' unless input[-1..-1] == '/'
-	until File.directory?(input) && Dir.glob("#{input}*.flv") != []
+	dir =  dir || gets.chomp
+	dir = normalize_directory(dir)
+	until File.directory?(dir) && Dir.glob("#{dir}*.flv") != []
 		puts "That directory either doesn't exist or contains no .flv files. \nEnter the full directory path of the flv files."
-		input = $stdin.gets.chomp
-		input.gsub!('\\', '/')
-		input += '/' unless input[-1..-1] == '/'
+		dir = $stdin.gets.chomp
+		dir = normalize_directory(dir)
 	end
-	dir = input
+
+	dir
 end
 
-### For each .flv in the directory create an html document named
-### like the flv, edit the default embed code, and write it to a file.
-### Also creates a .txt with a list of all the URLs for the files in that folder.
+def output_html_wrapper(flv_filename, output_folder)
+  html_filename = flv_filename.gsub(".flv", ".html")
 
-def output(flv, location)
-	title = flv.dup.gsub!(".flv", ".html")
-	vid = flv.dup
-	vid.slice!(0..6)
-	body = $EMBED.gsub("sample.flv", vid) 
-	htmlOutput = File.open(title, "w")
-		htmlOutput.write(body)
-	htmlOutput.close
-	linkList = File.open("#{location}List of Links.txt", "a")
-		linkList.write($BASE + vid.gsub(".flv", ".html") + "\n")
-	linkList.close
-	puts "Files created successfully." # Kind of unecessary as this will print as long as the directory is valid even if no files are created, I think.
+  html_body = $EMBED_HTML.gsub("sample.flv", flv_filename.slice(7..flv_filename.length)) 
+  html_output = File.open(html_filename, "w")
+  html_output.write(html_body)
+  html_output.close
+
+  link = flv_filename.slice(2..flv_filename.length).gsub(".flv", ".html")
+  link_list = File.open("#{output_folder}List of Links.txt", "a")
+  link_list.write($BASE_URL + link + "\n")
+  link_list.close
+	
+  puts "#{html_filename} created successfully." if File.exists?(html_filename)
 end
 
-### The script
-
-dir = ARGV[0].dup unless ARGV.empty?		# Gets file location if not provided in as command line argument.
-folder = validate_directory(dir)			# Creates a new instance of the Cleaner class and calls the is_valid_dir method.
-files = folder.clone + "*.flv"				# Wildcard query for all .flv's in the folder entered by the user.
-flvs = Dir.glob("#{files}") 				# Creates an array of all .flvs files in the directory.
+folder = ARGV[0].dup unless ARGV.empty?
+folder = validate_directory(folder)
+flvs = Dir.glob("#{folder}*.flv") 
 File.delete("#{folder}List of Links.txt") if File.exists?("#{folder}List of Links.txt")
-flvs.each { |flv| output(flv, folder) }		# Send each flv file through the my output method.
+flvs.each { |flv| output_html_wrapper(flv, folder) }
